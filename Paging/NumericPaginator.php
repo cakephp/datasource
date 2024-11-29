fb75b79d20795c83c57d0089f8f86d3f23b2bcf0
Mark Scherer <dereuromark@users.noreply.github.com>
2024-11-29 17:11:34 +0100

    Warn about non applied pagination config (#18043)
    Warn about non applied pagination config

--- Paging/NumericPaginator.php
+++ Paging/NumericPaginator.php
@@ -274,6 +274,12 @@ class NumericPaginator implements PaginatorInterface
             }
 
             $query = $object->find($type, ...$args);
+        } elseif (!empty($options['finder'])) {
+            triggerWarning(sprintf(
+                'Finder option (`%s`) from pagination config is not applied'
+                        . ' when a `SelectQuery` instance is passed to `paginate()`',
+                $options['finder'],
+            ));
         }
 
         $query->applyOptions($queryOptions);
